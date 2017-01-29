c2d2308bef57cc0a4c8f8502e3440c783c0f2e7b
Saurabh Kaushik <kaushiksaurabh17@yahoo.com>
2017-01-29 15:25:35 +0530

    parkator demo

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -2,30 +2,30 @@
 
 module.exports.homelist = function(req, res) {
   res.render('locations-list', {
-      title: "Loc8r: find a place to work with wifi",
+      title: "Parkator: find a place to park your car",
       pageHeader: {
-          title: "Loc8r",
-          strapline: "Find places to work with wifi near you!"
+          title: "Parkator",
+          strapline: "Find places to park your car, wherever you are!"
       },
-      sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
+      sidebar: "Looking for a parking space near you? Parkator helps you find places to park when out and about. Perhaps need a break from a long drive? Let Parkator help you find the place you're looking for.",
       locations: [{
-          name: "Starcups",
-          address: "125 High Street, Reading, RG6 1PS",
-          rating: 3,
-          facilities: ["Hot drinks", "Food", "Premium wifi"],
-          distance: "100m"
-      },{
-          name: "Cafe Hero",
-          address: "125 High Street, Reading, RG6 1PS",
-          rating: 4,
-          facilities: ["Hot drinks", "Food", "Premium wifi"],
-          distance: "200m"
-      },{
-          name: "Burger Queen",
-          address: "125 High Street, Reading, RG6 1PS",
-          rating:2 ,
-          facilities: ["Hot drinks", "Food"],
+          name: "Select Citywalk",
+          address: "A-3 District Centre, Saket, Sector 6, Pushp Vihar, New Delhi-110017",
+          rating: 5,
+          facilities: ["Drinks", "Food", "Shopping"],
           distance: "250m"
+      },{
+          name: "PVR Saket",
+          address: "A-3 District Centre, Saket, Sector 6, Pushp Vihar, New Delhi-110017",
+          rating: 4,
+          facilities: ["Drinks", "Food", "Movie"],
+          distance: "500m"
+      },{
+          name: "DLF Place Saket",
+          address: "A-3 District Centre, Saket, Sector 6, Pushp Vihar, New Delhi-110017",
+          rating: 3,
+          facilities: ["Drinks", "Food", "Movie", "Shopping"],
+          distance: "300m"
       },]
   });  
 };
@@ -34,47 +34,52 @@ module.exports.homelist = function(req, res) {
 
 module.exports.locationInfo = function(req, res) {
   res.render('location-info', {
-      title: 'Starcups',
+      title: 'Select Citywalk',
         pageHeader: {
-            title: 'Starcups'
+            title: 'Select Citywalk'
         },
         sidebar: {
-            context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
+            context: 'is on Parkator because it has accessible parking and space to relax, have a break, eat sumptuous and shop.',
             callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
         },
         location: {
-            name: 'Starcups',
-            address: '125 High Street, Reading, RG6 1PS',
+            name: 'Select Citywalk',
+            address: 'A-3 District Centre, Saket, Sector 6, Pushp Vihar, New Delhi-110017',
             rating: 3,
-            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            facilities: ["Drinks", "Food", "Shopping"],
             coords: {
-                lat: 51.455041,
-                lng: -0.9690884
+                lat: 28.5286024,
+                lng: 77.2170676
             },
             openingTimes: [{
                 days: 'Monday - Friday',
-                opening: '7:00am',
-                closing: '7:00pm',
+                opening: '08:00am',
+                closing: '01:00am',
                 closed: false
             }, {
                 days: 'Saturday',
-                opening: '8:00am',
-                closing: '5:00pm',
+                opening: '06:00am',
+                closing: '12:00am',
                 closed: false
             }, {
                 days: 'Sunday',
                 closed: true
             }],
             reviews: [{
-                author: 'Simon Holmes',
+                author: 'Kumud Shekhar',
                 rating: 5,
-                timestamp: '16 July 2013',
+                timestamp: '16 January 2017',
                 reviewText: 'What a great place. I can\'t say enough good things about it.'
             }, {
-                author: 'Charlie Chaplin',
+                author: 'Gaurav Chawla',
                 rating: 3,
-                timestamp: '16 June 2013',
+                timestamp: '16 December 2016',
                 reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
+            }, {
+                author: 'Saurabh Kaushik',
+                rating: 5,
+                timestamp: '16 July 2016',
+                reviewText: 'What a great place. I can\'t say enough good things about it.'
             }]
         }
   });  
@@ -84,9 +89,9 @@ module.exports.locationInfo = function(req, res) {
 
 module.exports.addReview = function(req, res) {
   res.render('location-review-form', {
-      title: 'Review Starcups on Loc8r',
+      title: 'Review Select Citywalk on Parkator',
       pageHeader: {
-        title: 'Review Starcups'
+        title: 'Review Select Citywalk'
       }
   });  
 };
